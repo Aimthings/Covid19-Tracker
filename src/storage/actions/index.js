db09c15f8b8ef53b17ad4d8eb4ea2dfca19136a1
Amit Singh <70027376+Amit-Singh1916@users.noreply.github.com>
2021-02-09 15:55:44 +0530

    Update index.js

--- src/storage/actions/index.js
+++ src/storage/actions/index.js
@@ -1,5 +1,6 @@
 import BaseUrl from '../../utils/config/BaseUrl';
 import { tabledatatoarr } from '../../utils/MaptoArr';
+
 export const CovidData = () => async dispatch => {
   const response = await BaseUrl.get('/v4/min/data.min.json');
   const data = tabledatatoarr(response.data);
