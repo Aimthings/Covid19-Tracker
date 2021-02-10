66634df0eaf6eab2d3f9744206a62289b7da8151
Amit-Singh <62666888+amit1916@users.noreply.github.com>
2021-02-10 13:44:46 +0530

    updated placeholder

--- src/common/SearchBar/index.js
+++ src/common/SearchBar/index.js
@@ -7,7 +7,7 @@ import './searchBar.css';
 const SearchBar = ({ onChange, suggested, value }) => {
   return (
     <div className="sb401SearchBar">
-      <input onChange={onChange} type="text" label="Search State" placeholder="Search the State" value={value} suggested={suggested} />
+      <input onChange={onChange} type="text" label="Search State" placeholder="Search the State/UT" value={value} suggested={suggested} />
       {suggested}
       <div className="sb411searchicon">
         <i><FontAwesomeIcon icon="search" size="2x" className="sb411fontawesomeicon" /></i>
